5aa51112eba3f3b76faaeb0af7923691a825acfb
bvaic <vaiciulis.ben@gmail.com>
2026-03-07 15:29:48 -0600

    hello world

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen( 3000, '0.0.0.0', () => {
+    console.log(`App listening on port ${port}`);
+});
